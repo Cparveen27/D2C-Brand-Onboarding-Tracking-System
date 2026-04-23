a8c07ac5e769810cc2b216f470aaa53aed49ea8f
Parveen C <parru495@gmail.com>
2026-04-23 19:28:34 +0530

    Change database connection password
    Updated database connection password to a placeholder.

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Cparveen$123",
+    password: "your_password",
     database: "brand_system"
 });
 
